4face72fc9457765a044af513de8642e0b89eec5
manshad <manshadchangampalli010@gmail.com>
2021-02-02 19:22:39 +0530

    finished the calculator work

--- calculator.js
+++ calculator.js
@@ -2,4 +2,14 @@ function btnclick(val)
 {
 
     document.getElementById("screen").value+=val
+
+}
+function clearscreen()
+{
+    document.getElementById("screen").value=""
+}
+function equalclick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById('screen').value=result
 }
